261d783a8734ae54f044d993450b51c78a0ccc42
SriNivas <sreenivasyandamuri44@gmail.com>
2020-11-10 12:23:09 +0530

    auto scaling

--- asg.tf
+++ asg.tf
@@ -1,9 +1,19 @@
-resource "aws_launch_template" "" {
-  image_id = ""
-  instance_type = ""
+//inorder to create auto-scaling-group -- u requiredd to do lunch-template
+resource "aws_launch_template" "template" {
+  name = "frontend"
+  image_id = data.aws_ami.nginx-ami.id
+  instance_type = var.INSTANCE_TYPE
+  key_name = var.KEY_NAME
+  vpc_security_group_ids = [aws_security_group.allow-frontend-instance.id]  # have to open 22 port to get connection so to create security groups--sg.tf
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "test"
+    }
+  }
 }
-resource "aws_autoscaling_group" "" {
-  availability_zones = ["us-east-2"]
+
+resource "aws_autoscaling_group" "asg" {
   max_size = 0
   min_size = 0
 }
